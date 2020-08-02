88dc5513359be97d0bc2b0efea3d6e94f3085fc9
Alejandro Mongua <alejandromongua@tecnimaq.com>
2020-08-02 12:00:39 -0500

    Se agrega: * Puntuación * Meta de puntuación * Pausa * Mensajes en la aplicación

--- src/components/Juego.js
+++ src/components/Juego.js
@@ -12,10 +12,9 @@ const tickInterval = 20
 
 const reboteJugador1 = anchoJugador + offsetJugador
 
+const posicionInicial = 100
 class Juego extends React.Component {
   constructor (props) {
-    const posicionInicial = 100
-
     super(props)
 
     this.width = props.width
@@ -30,31 +29,23 @@ class Juego extends React.Component {
         velocidadY: velocidadInicialPelota
       },
       j1: posicionInicial,
-      j2: posicionInicial
+      j2: posicionInicial,
+      puntuacionj1: 0,
+      puntuacionj2: 0,
+      meta: 10,
+      text: 'Press space to START'
     }
   }
 
   componentDidMount () {
-    var intervalId = setInterval(this.timer.bind(this), tickInterval)
-    // almacenamos intervalId en el estado para que pueda ser detenido más tarde
-    this.setState((estadoActual) => ({
-      ...estadoActual,
-      intervalId
-    }))
-
     // Capturar el teclado
     document.addEventListener('keydown', this.teclaOprimida.bind(this))
     document.addEventListener('keyup', this.teclaSoltada.bind(this))
   }
 
-  componentWillUnmount () {
-    // use intervalId desde el estado para eliminar intervalo
-    clearInterval(this.state.intervalId)
-  }
-
   teclaOprimida (evento) {
     this.setState((estadoActual) => {
-      const estado = { ...estadoActual }
+      let estado = { ...estadoActual }
       switch (evento.keyCode) {
         case 38:
           estado.subirj2 = true
@@ -67,6 +58,37 @@ class Juego extends React.Component {
           break
         case 87:
           estado.subirj1 = true
+          break
+        case 32:
+          if (estado.intervalId) {
+            // Pausa
+            clearInterval(estado.intervalId)
+            estado.intervalId = 0
+            estado.text = 'Press space to CONTINUE'
+          } else if (estado.puntuacionj1 < estado.meta && estado.puntuacionj1 < estado.meta) {
+            // Despausa
+            // almacenamos intervalId en el estado para que pueda ser detenido más tarde
+            estado.intervalId = setInterval(this.timer.bind(this), tickInterval)
+            estado.text = ''
+          }
+          break
+        case 27 :
+          clearInterval(estado.intervalId)
+          estado = {
+            pelota: {
+              positionX: this.width / 2,
+              positionY: this.height / 2,
+              velocidadX: velocidadInicialPelota,
+              velocidadY: velocidadInicialPelota
+            },
+            j1: posicionInicial,
+            j2: posicionInicial,
+            puntuacionj1: 0,
+            puntuacionj2: 0,
+            intervalId: 0,
+            meta: 10,
+            text: 'Press space to START'
+          }
           break
       }
 
@@ -125,40 +147,72 @@ class Juego extends React.Component {
         estado.pelota.positionY = estadoActual.pelota.positionY + estado.pelota.velocidadY
       }
 
-      if (estado.subirj1) {
+      if (estado.subirj1 && estado.j1 > 0) {
         estado.j1 = estado.j1 - velocidadJugador
       }
 
-      if (estado.subirj2) {
+      if (estado.subirj2 && estado.j2 > 0) {
         estado.j2 = estado.j2 - velocidadJugador
       }
 
-      if (estado.bajarj1) {
+      if (estado.bajarj1 && estado.j1 < this.height - altoJugador) {
         estado.j1 = estado.j1 + velocidadJugador
       }
 
-      if (estado.bajarj2) {
+      if (estado.bajarj2 && estado.j2 < this.height - altoJugador) {
         estado.j2 = estado.j2 + velocidadJugador
       }
-
+      if (estado.pelota.positionX >= this.width) {
+        estado.puntuacionj1++
+        if (estado.puntuacionj1 === estado.meta) {
+          estado.text = 'GAME OVER P1 WINS. Press ESC to RESTART'
+          clearInterval(estado.intervalId)
+          estado.intervalId = 0
+        }
+        estado.pelota.velocidadX = -estado.pelota.velocidadX
+        estado.pelota.positionX = this.width / 2
+        estado.pelota.positionY = this.height / 2
+      }
+      if (estado.pelota.positionX <= 0) {
+        estado.puntuacionj2++
+        if (estado.puntuacionj2 === estado.meta) {
+          estado.text = 'GAME OVER P2 WINS. Press ESC to RESTART'
+          clearInterval(estado.intervalId)
+          estado.intervalId = 0
+        }
+        estado.pelota.velocidadX = -estado.pelota.velocidadX
+        estado.pelota.positionX = this.width / 2
+        estado.pelota.positionY = this.height / 2
+      }
       return estado
     })
   }
 
   render () {
     return (
-      <svg width={this.width} height={this.height}>
-        <rect x={offsetJugador} y={this.state.j1} width={anchoJugador} height={altoJugador} fill={colorJugadorPelota} />
-        <rect x={this.posicionXJugador2} y={this.state.j2} width={anchoJugador} height={altoJugador} fill={colorJugadorPelota} />
-        <circle cx={this.state.pelota.positionX} cy={this.state.pelota.positionY} r={radioPelota} fill={colorJugadorPelota} />
-      </svg>
+      <div>
+        <input value={this.state.meta}/>
+        <div>
+          <svg width={this.width} height={this.height}>
+            <rect x={offsetJugador} y={this.state.j1} width={anchoJugador} height={altoJugador} fill={colorJugadorPelota} />
+            <rect x={this.posicionXJugador2} y={this.state.j2} width={anchoJugador} height={altoJugador} fill={colorJugadorPelota} />
+            <circle cx={this.state.pelota.positionX} cy={this.state.pelota.positionY} r={radioPelota} fill={colorJugadorPelota} />
+            {
+              this.state.text
+                ? <text x="5" y="15" fill="red">{this.state.text}</text>
+                : ''
+            }
+          </svg>
+        </div>
+        <h2>{this.state.puntuacionj1} - {this.state.puntuacionj2}</h2>
+      </div>
     )
   }
 }
 
-Juego.propTypes = { 
+Juego.propTypes = {
   width: PropTypes.number,
-  height: PropTypes.number,
- }
+  height: PropTypes.number
+}
 
 export default Juego
